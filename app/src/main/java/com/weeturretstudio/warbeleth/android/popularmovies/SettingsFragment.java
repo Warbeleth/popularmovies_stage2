15e01396cb57ed011ed40c81944a8c68031d4d51
Corey Ringer <elementalninja@gmail.com>
2018-07-18 23:47:04 -0400

    Incremental Progress!
    SettingsFragment.java
    *Populated initial functions and fields for SettingsFragment
    
    activity_settings.xml
    *Modified settings to be a fragment in place of a constraint layout.
    
    movie_type_preference.xml
    *Added summary off fields.

--- app/src/main/java/com/weeturretstudio/warbeleth/android/popularmovies/SettingsFragment.java
+++ app/src/main/java/com/weeturretstudio/warbeleth/android/popularmovies/SettingsFragment.java
@@ -1,11 +1,54 @@
 package com.weeturretstudio.warbeleth.android.popularmovies;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.Preference;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.movie_type_preference);
 
+        //SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        //PreferenceScreen prefScreen = getPreferenceScreen();
+        //int count = prefScreen.getPreferenceCount();
+
+        //If we weren't using checkboxes, we'd iterate over the preferences like we did in lesson 6.10
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //Identify the changed preference
+        Preference preference = findPreference(key);
+
+        if(null != preference) {
+            //Update summary
+            if(preference instanceof CheckBoxPreference) {
+                Log.v("Temp", "Key: " + preference.getKey() + " Value: " + preference.toString());
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
